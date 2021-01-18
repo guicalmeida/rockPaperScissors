02643743ae824c3f6db74e6ed659d2df0d7765e6
guicalmeida <guica.almeida@gmail.com>
2021-01-18 14:41:40 -0300

    working UI

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let buttonDiv = document.querySelector("#options"),
-    gameButtons = buttonDiv.querySelectorAll("button"),
+    gameButtons = buttonDiv.querySelectorAll(".button"),
     gameText = document.querySelector("#gameText"),
+    start = document.querySelector("#start"),
+    scoreboard = document.querySelector("#scoreboard"),
+    playerCurrentScore = document.querySelector("#humanScore"),
+    computerCurrentScore = document.querySelector("#AIScore"),
     playerScore = 0,
     computerScore = 0;
 
@@ -16,31 +20,44 @@ gameButtons.forEach(button => {
         })
     })
 
+buttonDiv.style.display = "none";
+scoreboard.style.display = "none";
+
 function playRound(playerSelection, computerSelection) {
 
     function buttonPlay (winner) {
         if (playerScore == 5 || computerScore == 5) {
-            console.log(playerScore + " AI " + computerScore);
             buttonDiv.style.display = "none";
             return gameText.textContent = `${winner} wins the Game! You played ${playerSelection} and the A.I. played ${computerSelection}.`
         } else {
-            console.log(playerScore + " AI " + computerScore);
-            return gameText.textContent = `${winner} wins the Round! \n You played ${playerSelection} and the A.I. played ${computerSelection}. your score: ${playerScore} \n A.I. score: ${computerScore}. \n Next round...`
+            return gameText.textContent = `${winner} wins the Round! \n You played ${playerSelection} and the A.I. played ${computerSelection}.`
         }
     }
 
     if ((playerSelection == "paper" && computerSelection == "rock") ||
         (playerSelection == "rock" && computerSelection == "scissors") ||
         (playerSelection == "scissors" && computerSelection == "paper")) {
-            playerScore +=1;
+            playerScore += 1;
+            playerCurrentScore.textContent = playerScore;
             return buttonPlay("player");
     } else if ((playerSelection == "paper" && computerSelection == "scissors") ||
         (playerSelection == "rock" && computerSelection == "paper") ||
         (playerSelection == "scissors" && computerSelection == "rock")) {
             computerScore += 1;
+            computerCurrentScore.textContent = computerScore;
             return buttonPlay("A.I.");
     } else {
-        console.log(playerScore + " AI " + computerScore);
-        return gameText.textContent = `It's a Tie! You played ${playerSelection} and the A.I. played ${computerSelection}. \n your score: ${playerScore} \n A.I. score: ${computerScore}. \n Next round...`;
+        return gameText.textContent = `It's a Tie! You played ${playerSelection} and the A.I. played ${computerSelection}.`;
     }
 }
+
+start.addEventListener("click", () => {
+    buttonDiv.style.display = "flex";
+    gameText.textContent = "Choose your fighter!";
+    computerCurrentScore.textContent = 0;
+    playerCurrentScore.textContent = 0;
+    start.style.display = "none";
+    scoreboard.style.display = "flex";
+
+})
+
